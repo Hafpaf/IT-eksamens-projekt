901e11b81c05c285265f5f22acef99915131a0dc
hafpaf <haf@hafnium.me>
2019-03-12 21:00:44 +0100

    add comments

--- videocapture.py
+++ videocapture.py
@@ -6,15 +6,15 @@ cap = cv.VideoCapture(0) #select capture device, 0 is inbuild
 
 while True:
     #Capture frame by frame
-    ret, frame = cap.read()
+    ret, frame = cap.read() #ret: obtain value from source. Frame: get next frame
 
     #Frame proccessing
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) #BGR color scheme to GRAY color scheme
 
     #Display Frame
-    cv.imshow('frame',gray)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+    cv.imshow('frame',gray) #Display
+    if cv.waitKey(1) & 0xFF == ord('q'): #close by keypress q
+        break #exit loop
 
 cap.release()
 cv.destroyAllWindows()
